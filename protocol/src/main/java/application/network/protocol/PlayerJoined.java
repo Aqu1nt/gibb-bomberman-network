075660c84c200657e67743106ae9964a832c727f
andreas <andyfankhauser@gmx.ch>
2016-11-09 13:40:33 +0100

    Simple implementation for new message from last merging.

--- protocol/src/main/java/application/network/protocol/PlayerJoined.java
+++ protocol/src/main/java/application/network/protocol/PlayerJoined.java
@@ -3,8 +3,44 @@ package application.network.protocol;
 import application.network.api.Message;
 
 
+/**
+ * Diese Nachricht wird vom Server gesendet um den Clients mitzuteilen, dass ein ein Neuer Spieler im Spiel ist.
+ */
 public class PlayerJoined implements Message {
 
-    // Has: playerName, initialX and initialY
+
+    private String playerName;
+
+    private int positionX;
+
+    private int positionY;
+
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public PlayerJoined setPlayerName(String playerName) {
+        this.playerName = playerName;
+        return this;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public PlayerJoined setPositionX(int positionX) {
+        this.positionX = positionX;
+        return this;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public PlayerJoined setPositionY(int positionY) {
+        this.positionY = positionY;
+        return this;
+    }
 
 }

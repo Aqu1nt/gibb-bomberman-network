9e9caafeb03066b895bb7f2c7504897df9dddafb
andreas <andyfankhauser@gmx.ch>
2016-11-23 21:09:28 +0100

    NetworkModule does now return instances.

--- network-impl-a/src/main/java/application/network/impl/a/NetworkModuleA.java
+++ network-impl-a/src/main/java/application/network/impl/a/NetworkModuleA.java
@@ -4,20 +4,20 @@ import application.network.api.NetworkModule;
 import application.network.api.client.ServerProxy;
 import application.network.api.server.Server;
 
+
 /**
  * Implementierung des Netzwerk Modules der Gruppe A!
  */
-public class NetworkModuleA implements NetworkModule
-{
+public class NetworkModuleA implements NetworkModule {
+
     @Override
-    public Server createServer()
-    {
-        return null;
+    public Server createServer() {
+        return new ServerImpl();
     }
 
     @Override
-    public ServerProxy createClient()
-    {
-        return null;
+    public ServerProxy createClient() {
+        return new ClientImpl();
     }
+
 }
